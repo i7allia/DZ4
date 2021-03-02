9904be0b03b2bade4b85df4cc7468debde0cc440
i7allia <76435852+i7allia@users.noreply.github.com>
2021-03-02 11:26:46 +0300

    Создал таблицу GoodsSales

--- MallDB.sql
+++ MallDB.sql
@@ -1,6 +1,7 @@
-CREATE TABLE GoodsGroups
+CREATE TABLE GoodsSales
 (
 id INT NOT NULL,
-Name VARCHAR(10) NOT NULL,
-GoodGroup INT NOT NULL
+good_id INT NOT NULL,
+quantity INT,
+Sale_date DATE NOT NULL,
 )
